5fd7c6f268540bd51816e224cdac4c379ca8cbc4
pivanta <bas677@icloud.com>
2021-01-03 23:30:51 +0000

    Final product

--- app.js
+++ app.js
@@ -9,39 +9,37 @@ var potatoe = new Items("Potatoe", 1, 2.99, []);
 var cabbage = new Items("Cabbage", 1, 1.19, []);
 var parsnip = new Items("Parsnip", 1, 0.89, []);
 var tomatoe = new Items("Tomatoe", 1, 1.49, []);
-var okra = new Items("Okra", 1, "2.20", []);
+var okra = new Items("Okra", 1, 2.21, []);
 var cucumber = new Items("Cucumber", 1, 0.99, []);
 var onion = new Items("Onion", 1, 1.59, []);
-var carrot = new Items("Carrot", 1, "1.00", []);
+var carrot = new Items("Carrot", 1, 1.09, []);
 
 var totalPriceCheck = [];
 var totalQtyCheck = [];
-var duplicateItemNameCheck = [];
-var qtyIncrement ;
+var qtyIncrement = [];
 
 //  Bill total function
 function billCalc() {
   var total = totalPriceCheck.reduce((a, b) => a + b, 0);
-  document.getElementById("totalAmount").innerHTML =
-    "£" + total.toFixed(2);
+  return (document.getElementById("totalAmount").innerHTML =
+    "£" + total.toFixed(2));
 }
 // Bill quantity total function
 function qtyCalc() {
   var total = totalQtyCheck.reduce((a, b) => a + b, 0);
-  return document.getElementById("totalQty").innerHTML = total;
+  return (document.getElementById("totalQty").innerHTML = total);
 }
-
 function addItems(itemName, qty, price, arr) {
+  // ADD ITEMS
   var node = document.createElement("TR");
   var textnode = document.createTextNode(itemName);
-  duplicateItemNameCheck.push(textnode);
   node.appendChild(textnode);
   document
     .getElementById("itemName")
     .appendChild(node)
     .setAttribute("class", itemName);
-  console.log(duplicateItemNameCheck);
 
+  // ADD QTY
   var node1 = document.createElement("TR");
   var textnode1 = document.createTextNode(qty);
   qtyConvert = Number(qty);
@@ -54,6 +52,7 @@ function addItems(itemName, qty, price, arr) {
   arr.push(qtyConvert);
   qtyCalc();
 
+  // ADD PRICE
   var node2 = document.createElement("TR");
   var textnode2 = document.createTextNode("£" + price);
   var numConvert = Number(price);
@@ -64,32 +63,32 @@ function addItems(itemName, qty, price, arr) {
     .setAttribute("class", itemName);
   totalPriceCheck.push(numConvert);
   billCalc();
-  console.log(totalPriceCheck);
 }
 function multiplier(objName, className) {
-    qtyIncrement = objName.arr.push(1);
-    totalQtyCheck.push(1);
-    totalPriceCheck.push(objName.price);
+  qtyIncrement = objName.arr.push(1);
+  totalQtyCheck.push(1);
+  totalPriceCheck.push(objName.price);
   var getQtyClass = document.getElementById("qty");
-    getQtyClass.querySelector(className).innerHTML = qtyIncrement;
+  getQtyClass.querySelector(className).innerHTML = qtyIncrement;
   var getPriceClass = document.getElementById("price");
   var multiplierPrice = qtyIncrement * objName.price;
-    getPriceClass.querySelector(className).innerHTML = "£" + multiplierPrice.toFixed(2);
-    qtyCalc();
-    billCalc();
+  getPriceClass.querySelector(className).innerHTML =
+    "£" + multiplierPrice.toFixed(2);
+  qtyCalc();
+  billCalc();
 }
 
 function potatoes() {
   if (potatoe.arr.length > 0) {
-    multiplier(potatoe,".Potatoe");
+    multiplier(potatoe, ".Potatoe");
   } else {
     addItems(potatoe.name, potatoe.qty, potatoe.price, potatoe.arr);
-  } 
+  }
 }
 
 function cabbages() {
   if (cabbage.arr.length > 0) {
-    multiplier(cabbage,".Cabbage");
+    multiplier(cabbage, ".Cabbage");
   } else {
     addItems(cabbage.name, cabbage.qty, cabbage.price, cabbage.arr);
   }
@@ -97,48 +96,48 @@ function cabbages() {
 
 function parsnips() {
   if (parsnip.arr.length > 0) {
-    multiplier(parsnip,".Parsnip");
+    multiplier(parsnip, ".Parsnip");
   } else {
-      addItems(parsnip.name, parsnip.qty, parsnip.price, parsnip.arr);
-    }
+    addItems(parsnip.name, parsnip.qty, parsnip.price, parsnip.arr);
+  }
 }
 
 function tomatoes() {
   if (tomatoe.arr.length > 0) {
-    multiplier(tomatoe,".Tomatoe");
+    multiplier(tomatoe, ".Tomatoe");
   } else {
-      addItems(tomatoe.name, tomatoe.qty, tomatoe.price, tomatoe.arr);
-    }
+    addItems(tomatoe.name, tomatoe.qty, tomatoe.price, tomatoe.arr);
+  }
 }
 
 function okras() {
   if (okra.arr.length > 0) {
-    multiplier(okra,".Okra");
+    multiplier(okra, ".Okra");
   } else {
-      addItems(okra.name, okra.qty, okra.price, okra.arr);
-    }
+    addItems(okra.name, okra.qty, okra.price, okra.arr);
+  }
 }
 
 function cucumbers() {
   if (cucumber.arr.length > 0) {
-    multiplier(cucumber,".Cucumber");
+    multiplier(cucumber, ".Cucumber");
   } else {
-      addItems(cucumber.name, cucumber.qty, cucumber.price, cucumber.arr);
-    }
+    addItems(cucumber.name, cucumber.qty, cucumber.price, cucumber.arr);
+  }
 }
 
 function onions() {
   if (onion.arr.length > 0) {
-    multiplier(onion,".Onion");
+    multiplier(onion, ".Onion");
   } else {
-      addItems(onion.name, onion.qty, onion.price, onion.arr);
-    }
+    addItems(onion.name, onion.qty, onion.price, onion.arr);
+  }
 }
 
 function carrots() {
   if (carrot.arr.length > 0) {
-    multiplier(carrot,".Carrot");
+    multiplier(carrot, ".Carrot");
   } else {
-      addItems(carrot.name, carrot.qty, carrot.price, carrot.arr);
-    }
+    addItems(carrot.name, carrot.qty, carrot.price, carrot.arr);
+  }
 }
